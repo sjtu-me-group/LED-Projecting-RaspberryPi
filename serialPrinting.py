389276524836ae051e7de6a4c2af2551977a9694
Mintpasokon <2518558815@qq.com>
2019-06-26 15:32:12 +0800

    Update serialPrinting.py

--- serialPrinting.py
+++ serialPrinting.py
@@ -4,5 +4,5 @@ import os
 if __name__ == '__main__':
   def sprint(input):
     os.system('sudo chmod a+w /dev/usb/lp0')
-    shellPast = "sudo echo -e " + str(input)+ "> /dev/usb/lp0"
+    shellPast = "sudo echo -e " + repr(str(input))+ "> /dev/usb/lp0"
     os.system(shellPast)
